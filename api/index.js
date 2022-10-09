a2b3869b99f77aa08fef38fb73e0390e07001188
Tuibaev Jalyn <sharrder.blendi.1@gmail.com>
2022-10-09 22:53:27 +0600

    Настроил api, websocket

--- api/index.js
+++ api/index.js
@@ -8,8 +8,33 @@ const port = 8000;
 
 app.use(cors());
 
-app.ws('/canvas', (ws, req) => {
+const activeConnections = {};
+let pixelsArray = [];
 
+app.ws('/canvas', (ws) => {
+    const id = nanoid();
+
+    console.log('Client connected id=', id);
+    activeConnections[id] = ws;
+
+    if (pixelsArray.length > 0) {
+        ws.send(pixelsArray);
+    }
+
+    ws.on('close', () => {
+        console.log('Client disconnected! id=', id);
+        delete activeConnections[id];
+    });
+
+
+    ws.on('message', drawing => {
+        Object.keys(activeConnections).forEach(connId => {
+            const conn = activeConnections[connId];
+
+            pixelsArray = drawing;
+            conn.send(drawing);
+        });
+    });
 });
 
 app.listen(port, () => {
